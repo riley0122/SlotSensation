e47e080030b8265fd6e579075edfb7d48472a9cf
LucasAmmer <lucasammerlaan123@gmail.com>
2023-10-21 23:32:22 +0200

    added removing old commands

--- deploy.js
+++ deploy.js
@@ -27,6 +27,18 @@ for (const command in cmdFolder) {
   }
 }
 
+// Remove all old commands
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+  .then(() => console.log("Successfully deleted all guild commands."))
+  .catch(console.error);
+
+// for global commands
+rest
+  .put(Routes.applicationCommands(clientId), { body: [] })
+  .then(() => console.log("Successfully deleted all application commands."))
+  .catch(console.error);
+
 // deploy them
 const rest = new REST().setToken(token);
 
